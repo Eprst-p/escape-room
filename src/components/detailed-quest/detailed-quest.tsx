d5152eb864f96953a5b4161b69c4d9b6d4262dbe
Eprst <easter_p@mail.ru>
2022-04-14 12:54:14 +0300

    переход на TS, кажется работает

--- src/components/detailed-quest/detailed-quest.tsx
+++ src/components/detailed-quest/detailed-quest.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { MainLayout } from 'components/common/common';
-import IconClock from 'assets/img/icon-clock.svg';
-import IconPerson from 'assets/img/icon-person.svg';
-import IconPuzzle from 'assets/img/icon-puzzle.svg';
+import {ReactComponent as IconClock} from 'assets/img/icon-clock.svg';
+import {ReactComponent as IconPerson} from 'assets/img/icon-person.svg';
+import {ReactComponent as IconPuzzle} from 'assets/img/icon-puzzle.svg';
 import * as S from './detailed-quest.styled';
 import { BookingModal } from './components/components';
 
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,15 +1,10 @@
 
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
+// declare module "*.svg" {
+//   const content: any;
+//   export default content;
+// }
 
-declare module "*.jpg" {
-  const content: any;
-  export default content;
-}
-
-// declare module "react-dom" {
-//   const content: Renderer;
+// declare module "*.jpg" {
+//   const content: any;
 //   export default content;
 // }
